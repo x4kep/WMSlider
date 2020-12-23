0377caa88c33e5bfb140d94c786b0fc89da49418
dusan.veselinovic.dev gmail.com <dusan.veselinovic.dev@gmail.com>
2020-12-23 12:12:28 +0100

    Stru: Html template Stru: JS some basic animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-(function(){
-  console.log('Hello world');
+(function () {
+  "use strict"
+
+  const slider = new Slider();
+
+  function Slider() {
+
+    let distance = 0;
+
+    let sliderSettings = {
+      refreshRate: 15,
+      distanceIncrementer : 1
+    }
+
+    const uiElements = {
+      SliderContainer: document.getElementsByClassName("slider")[0],
+      SliderRow: document.getElementsByClassName("slider__row")[0]
+    }
+
+    setInterval(function () {
+      distance += sliderSettings.distanceIncrementer;
+      uiElements.SliderRow.style.transform = "translateX(" + distance + "px)";
+      if (distance > 1200) { distance = 0; }
+    }, sliderSettings.refreshRate)
+
+  }
+
+
 }())
